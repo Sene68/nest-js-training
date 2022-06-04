24d29ec05efaef567a5b50090877d35c17f630fa
Sene68 <senefull68@gmail.com>
2022-06-04 12:42:54 +0900

    서비스 모듈 분리
    UserService / EmailService 모듈 분리

--- project-nest-s3/src/app.module.ts
+++ project-nest-s3/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { EmailService } from './email/email.service';
+import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, EmailService],
+  imports: [UsersModule, EmailModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
