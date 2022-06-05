c59e54d873457eb40d071b3688ef60ffa530218e
Sene68 <senefull68@gmail.com>
2022-06-05 10:27:33 +0900

    dotenv를 이용한 config 설정
    local만 표현되도록 구성

--- project-nest-s3/src/app.module.ts
+++ project-nest-s3/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UsersModule, EmailModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
