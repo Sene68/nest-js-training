abd6be62d7c88e8a865498592e9b56fbcc88c417
Sene68 <senefull68@gmail.com>
2022-06-06 10:32:30 +0900

    config 패키지 적용

--- project-nest-s3/src/app.controller.ts
+++ project-nest-s3/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, 
+    private readonly configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    return process.env.DATABASE_HOST;
+    return this.appService.getHello();
+  }
+
+  @Get('/db-host-from-config')
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get('DATABASE_HOST');
   }
 }
--- project-nest-s3/src/app.module.ts
+++ project-nest-s3/src/app.module.ts
@@ -3,10 +3,14 @@ import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env'
+      : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
+  }),UsersModule,EmailModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule { }
